c314fa3239eacb5008af591f3e514e31fda124c8
cchoseonghun <cchoseonghun@gmail.com>
2022-07-25 18:19:43 +0900

    apply redux-persist

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
   // </React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,46 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
 import user from './userSlice'
 import nav from './navSlice'
 import raid from './raidSlice'
 import modal from './modalSlice'
 import group from './groupSlice'
 
-export default configureStore({
-    reducer: {
-        user: user.reducer, 
-        nav: nav.reducer, 
-        raid: raid.reducer, 
-        modal: modal.reducer, 
-        group: group.reducer, 
-    }
-})
+// export default configureStore({
+//     reducer: {
+//         user: user.reducer, 
+//         nav: nav.reducer, 
+//         raid: raid.reducer, 
+//         modal: modal.reducer, 
+//         group: group.reducer, 
+//     }
+// })
+
+const reducers = combineReducers({
+    user: user.reducer, 
+    nav: nav.reducer, 
+    raid: raid.reducer, 
+    modal: modal.reducer, 
+    group: group.reducer, 
+});
+
+const persistConfig = {
+    key: 'root', 
+    storage: storage, 
+    // blacklist: [ 'group' ], 유지시키지 않을 리스트
+    // whitelist: [ 'group' ], 
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer, 
+    // devTools: 
+    middleware: [thunk], 
+});
+
+export default store;
